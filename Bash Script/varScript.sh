73dbe6375c37555638ce1d462330ca60186a6392
fareedaragab <66780788+farida52369@users.noreply.github.com>
2021-10-19 23:28:48 +0200

    Create varScript.sh

---
+++ Bash Script/varScript.sh
@@ -0,0 +1,15 @@
+# YOU DON'T HAVE TO WRITE THE NAME OF THE FILE .sh
+# TO GET THE BASH PATH 
+# $which bash (THE OUTPUT: /usr/bin/bash)
+# THE CREATING OF THE FILE WITHOUT EXCUTION 
+# $chmod +x fileName || $chmod 755 fileName
+# THEN TO RUN THE SCRIPT
+# $./fileName
+
+# THE CONTENT __ VARIABLES IN BASH SCRIPT
+
+#!/usr/bin/bash
+echo "Hello, "
+NAME="Fareeda Ragab"
+echo $NAME
+
